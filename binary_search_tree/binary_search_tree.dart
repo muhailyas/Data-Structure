76739bddb76c0a52437cb778159ddcde84a330bf
ilyas <muhammadilyas827827@gmail.com>
2024-02-06 09:29:09 +0530

    3 traversal added in bst

--- binary_search_tree/binary_search_tree.dart
+++ binary_search_tree/binary_search_tree.dart
@@ -39,64 +39,26 @@ class BinarySearchTree {
       return;
     }
     inOrder(root.left);
-    inOrder(root.right);
     stdout.write("${root.value} ");
+    inOrder(root.right);
   }
 
-  // delete node
-  void remover(int value) {
-    _removerHelper(value, null, root);
-  }
-
-  void _removerHelper(int target, Node? parentNode, Node? currentNode) {
-    while (currentNode != null) {
-      if (target < currentNode.value) {
-        parentNode = currentNode;
-        currentNode = currentNode.left;
-      } else if (target > currentNode.value) {
-        parentNode = currentNode;
-        currentNode = currentNode.right;
-      } else {
-        if (currentNode.left == null && currentNode.right == null) {
-          //finds the node
-          if (parentNode == null) {
-            root = null;
-            return;
-          } else if (parentNode.left == currentNode) {
-            parentNode.left = null;
-            return;
-          } else if (parentNode.right == currentNode) {
-            parentNode.right = null;
-            return;
-          }
-        } else if (currentNode.right == null) {
-          //when node have only left child
-          currentNode.value = _findLargest(currentNode.left);
-          _removerHelper(currentNode.value, currentNode, currentNode.left);
-        } else {
-          //if(currentNode.left == null) && current have left and right child
-          currentNode.value = _findSmallest(currentNode.right);
-          _removerHelper(currentNode.value, currentNode, currentNode.right);
-        }
-      }
+  preOrder(Node? root) {
+    if (root == null) {
+      return;
     }
-    return;
+    stdout.write("${root.value} ");
+    preOrder(root.left);
+    preOrder(root.right);
   }
 
-  //find smallest node in a bst
-  int _findSmallest(Node? currentNode) {
-    if (currentNode?.left == null)
-      return currentNode!.value;
-    else
-      return _findSmallest(currentNode!.left);
-  }
-
-  //find largest in a bst
-  int _findLargest(Node? currentNode) {
-    if (currentNode?.right == null)
-      return currentNode!.value;
-    else
-      return _findLargest(currentNode!.right);
+  postOrder(Node? root) {
+    if (root == null) {
+      return;
+    }
+    preOrder(root.left);
+    preOrder(root.right);
+    stdout.write("${root.value} ");
   }
 }
 
@@ -106,5 +68,10 @@ void main() {
   for (var element in list) {
     bst.insert(element);
   }
+  print("InOrder----------");
   bst.inOrder(bst.root);
+  print("\nPreOrder----------");
+  bst.preOrder(bst.root);
+  print("\nPostOrder----------");
+  bst.postOrder(bst.root);
 }
